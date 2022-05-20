11c508e953ad07cba9d1dd5afe5432162229315c
Nishmitha shetty <nishmitha.shetty@ymedialabs.com>
2022-05-20 11:50:26 +0530

    chore: add unit test cases to button component

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,18 +1,22 @@
-import { Component, Prop, h } from "@stencil/core";
+import { Component, Prop, Listen, h } from '@stencil/core';
 
 @Component({
-    tag: 'wc-button',
-    styleUrls: ['button.scss']
+  tag: 'wc-button',
+  styleUrls: ['button.scss'],
 })
-
 export class Button {
-    @Prop() label: string;
-    @Prop() eventHandler: () => void;
-    @Prop() variant: 'primary' | 'secondary';
+  @Prop() label: string;
+  @Prop() eventHandler: (ev: any) => void;
+  @Prop() variant: 'primary' | 'secondary';
 
-    render() {
-        return (
-            <button onClick={this.eventHandler} class={this.variant}>{this.label}</button>
-        )
-    }
+  @Listen('click', { capture: true })
+  handleClick(ev) {}
+
+  render() {
+    return (
+      <button onClick={this.handleClick} class={this.variant}>
+        {this.label}
+      </button>
+    );
+  }
 }
