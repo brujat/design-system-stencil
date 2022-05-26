14c306ed83437dcc42434c46a55248301c690a48
brujat <brujat.u@ymedialabs.com>
2022-05-26 12:13:40 +0530

    feat(eslint): add eslint

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -5,8 +5,11 @@ import { Component, Prop, Listen, h } from '@stencil/core';
   styleUrls: ['button.scss'],
 })
 export class Button {
+  /** @property{string} - label for the button */
   @Prop() label: string;
+  /** @property{function} - Button event handler */
   @Prop() eventHandler: (ev: any) => void;
+  /** @property{string} - Button variant type */
   @Prop() variant: 'primary' | 'secondary';
 
   @Listen('click', { capture: true })
